85b36c5643c802aa20a11c22554f546c6f4b64ec
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-08-24 23:32:09 +0530

    Done with docker errors

--- zipfile/firstjobapp/src/main/java/com/durga/firstjobapp/FirstjobappApplication.java
+++ zipfile/firstjobapp/src/main/java/com/durga/firstjobapp/FirstjobappApplication.java
@@ -3,11 +3,15 @@ package com.durga.firstjobapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FirstjobappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstjobappApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        System.out.println(java.util.TimeZone.getDefault());
+        SpringApplication.run(FirstjobappApplication.class, args);
 	}
 
 }
